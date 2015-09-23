884d5eaf67caecf38144c50d3cd2bb7a169d4edd
Yassir Oulmokhtar <yassirusa@gmail.com>
2015-09-23 08:49:42 -0400

    ttre3w

--- CameraExample/app/app.js
+++ CameraExample/app/app.js
@@ -9,8 +9,18 @@
 
         $scope.capturePhotoWithData = function () {
             if ($cordovaCamera == undefined) alert("$cordovaCamera is undefined");
-
-            $cordovaCamera.getPicture().then(function (imageData) {
+            var options = {
+                quality: 75,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 300,
+                targetHeight: 300,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false
+            };
+            $cordovaCamera.getPicture(options).then(function (imageData) {
                 var image = document.getElementById('myImage');
                 image.src = "data:image/jpeg;base64," + imageData;
             }, function (message) {
